1b49981abc581c6f4bf223121aa6160d057910af
Macwan Robert <macwanrobert04gmail.com>
2022-06-17 23:11:39 +0530

    Updates

---
+++ precache-manifest.bed0f0025e5c8629d87851b8bbdbc815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26caf720870222b99674",
+    "url": "/text-editor/static/css/main.2f11b6ff.chunk.css"
+  },
+  {
+    "revision": "26caf720870222b99674",
+    "url": "/text-editor/static/js/main.26caf720.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/text-editor/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "1bc61fac5efd88933d84",
+    "url": "/text-editor/static/js/2.1bc61fac.chunk.js"
+  },
+  {
+    "revision": "efe788aa2942c2a0e72f",
+    "url": "/text-editor/static/js/runtime~main.efe788aa.js"
+  },
+  {
+    "revision": "7d19c2109ae8644c2975eac9338b37f4",
+    "url": "/text-editor/index.html"
+  }
+];
